c1c738bbc041d508411259b796124cb3d30e17de
nguyenduckhoi <khoinguyenduc7@gmail.com>
2025-07-07 21:19:31 +0700

    feat: Like Post

--- socialmedia/src/main/java/Social/Media/Backend/Application/controller/LikeController.java
+++ socialmedia/src/main/java/Social/Media/Backend/Application/controller/LikeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/likes")
+@RequestMapping("/api/v1/like")
 @RequiredArgsConstructor
 public class LikeController {
 
