c8515e3dc993259c63e02d6d873a5076beecf834
nguyenduckhoi <khoinguyenduc7@gmail.com>
2025-07-10 21:57:03 +0700

    feat: Get All Friendship

--- socialmedia/src/main/java/Social/Media/Backend/Application/controller/FriendshipController.java
+++ socialmedia/src/main/java/Social/Media/Backend/Application/controller/FriendshipController.java
@@ -1,11 +1,28 @@
 package Social.Media.Backend.Application.controller;
 
+import Social.Media.Backend.Application.dto.response.ApiResponse;
+import Social.Media.Backend.Application.dto.response.FriendshipResponse;
+import Social.Media.Backend.Application.service.FriendshipService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/friendship")
 @RequiredArgsConstructor
 public class FriendshipController {
+
+    private final FriendshipService friendshipService;
+
+    @GetMapping
+    ApiResponse<List<FriendshipResponse>> getFriends() {
+        List<FriendshipResponse> friendshipResponseList = friendshipService.getFriendship();
+        return ApiResponse.<List<FriendshipResponse>>builder()
+                .code(1000)
+                .result(friendshipResponseList)
+                .build();
+    }
 }
